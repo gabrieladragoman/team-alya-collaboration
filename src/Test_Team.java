669ceed5e9da336dc79bc753b12b998caf67d8f1
Andjela Popovic <andjelapopov1@gmail.com>
2023-09-13 16:57:07 -0700

    B30

--- src/Test_Team.java
+++ src/Test_Team.java
@@ -2,5 +2,8 @@ public class Test_Team {
 
     public static void main(String[] args) {
         System.out.println("Batch 30, Casting group is the best!");
-    }
+
+        System.out.println("We can do this :) ");
+
+        }
 }
