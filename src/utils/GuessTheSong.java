e83ad522d4eb641271a4e6929379638729be11dc
Andjela Popovic <andjelapopov1@gmail.com>
2023-09-13 17:05:06 -0700

    Hint

--- src/utils/GuessTheSong.java
+++ src/utils/GuessTheSong.java
@@ -1,9 +1,12 @@
 package utils;
 
+import java.sql.SQLOutput;
+
 public class GuessTheSong {
 
     public static void main(String[] args) {
 
         System.out.println("In other words please be true...");
+        System.out.println("1. hint Fly :) ");
     }
 }
